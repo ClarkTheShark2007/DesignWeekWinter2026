bf7b7b0999d134f96523b07b3eae79d2eee62446
SrSiestas15 <148352083+SrSiestas15@users.noreply.github.com>
2026-03-03 13:02:33 -0500

    spawner completed

---
+++ Assets/Scripts/PhoneSpawner.cs
@@ -0,0 +1,60 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PhoneSpawner : MonoBehaviour
+{
+    public GameObject possibleLocationsParent;
+    public PhoneSpawnPoint[] spawnlocations;
+    public GameObject phonePrefab;
+
+    [SerializeField] float spawnTimer;
+    [SerializeField] float spawnPause;
+
+    private bool keepSpawning;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawnlocations = possibleLocationsParent.GetComponentsInChildren<PhoneSpawnPoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= spawnPause)
+        {
+            if (CheckIfEmpty())
+            {
+                PhoneSpawnPoint emptyPoint = FindEmptyPoint();
+                Instantiate(phonePrefab, emptyPoint.gameObject.transform);
+                emptyPoint.occupied = true;
+            }
+            spawnTimer = 0;
+        }
+    }
+
+    public bool CheckIfEmpty()
+    {
+        for(int i = 0; i < spawnlocations.Length; i++)
+        {
+            if (!spawnlocations[i].occupied) return true;
+        }
+        return false;
+    }
+
+    public PhoneSpawnPoint FindEmptyPoint()
+    {
+        PhoneSpawnPoint possiblePoint = spawnlocations[Random.Range(0, spawnlocations.Length)];
+        if (!possiblePoint.occupied)
+        {
+            return possiblePoint;
+        }
+        else
+        {
+            return FindEmptyPoint();
+        }
+    }
+}
