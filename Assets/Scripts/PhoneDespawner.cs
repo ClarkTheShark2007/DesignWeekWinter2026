589a45463b69e98e7d199de9282696cd98b0ff37
SrSiestas15 <148352083+SrSiestas15@users.noreply.github.com>
2026-03-05 16:02:42 -0500

    changes

--- Assets/Scripts/PhoneDespawner.cs
+++ Assets/Scripts/PhoneDespawner.cs
@@ -5,13 +5,20 @@ public class PhoneDespawner : MonoBehaviour
     int despawnTime = 30; // Time in seconds before the phone is despawned
     float currentTime = 0f;
 
+    PhoneController phoneController;
+
+    private void Start()
+    {
+        phoneController = GetComponent<PhoneController>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         currentTime = currentTime + Time.deltaTime;
         if (currentTime >= despawnTime)
         {
-            Destroy(gameObject); // Destroys the phone game object after the specified time
+            phoneController.PhoneDisappears();
         }
     }
 }
